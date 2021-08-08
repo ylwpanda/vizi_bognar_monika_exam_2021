3e94e8675777f234660240bbc988a153a0a9c2db
ylwpanda <monica.bognar@gmail.com>
2021-08-08 14:13:55 +0200

    ProfileMenu and ProfileMenuTest added.

---
+++ src/main/java/pages/ProfileMenu.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProfileMenu {
+
+    private WebDriver driver;
+    private final By PROFILE_BUTTON = By.xpath("//*[@id=\"imdbHeader\"]/div[2]/div[5]/div/label[2]/div/svg");
+
+    public ProfileMenu(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void selectFromProfileMenu(String menuitem){
+        findProfileMenuOption().selectByVisibleText(menuitem);
+    }
+
+    public List<String> getSelectedOptions() {
+        List<WebElement> selectedOptions = findProfileMenuOption().getAllSelectedOptions();
+        return selectedOptions.stream().map(e->e.getText()).collect(Collectors.toList());
+    }
+
+    private Select findProfileMenuOption(){
+        return new Select(driver.findElement(PROFILE_BUTTON));
+    }
+}
---
+++ src/test/java/profilemenu/ProfileMenuTest.java
@@ -0,0 +1,14 @@
+package profilemenu;
+
+import base.BaseTest;
+import org.junit.jupiter.api.Test;
+
+public class ProfileMenuTest extends BaseTest {
+
+    @Test
+    public void testProfileMenu() {
+        var profileMenu= homePage.clickProfileButton();
+        //profileMenu.selectFromProfileMenu(menuitem);
+    }
+
+}
