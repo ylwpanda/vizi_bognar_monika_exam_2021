19b265aaee4d27b48390813f60c15ed65b023819
ylwpanda <monica.bognar@gmail.com>
2021-08-18 19:35:29 +0200

    window-size=2560,1600

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -45,7 +45,7 @@ public class BaseTest {
         ChromeOptions.addArguments("--disable-dev-shm-usage");
         ChromeOptions.addArguments("--allow-insecure-localhost");
         ChromeOptions.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
-        ChromeOptions.addArguments("--window-size=1920,1080");
+        ChromeOptions.addArguments("--window-size=2560,1600");
         ChromeOptions.addArguments("--incognito");
         driver = new ChromeDriver(ChromeOptions);
         driver.get(TestData.URL);
